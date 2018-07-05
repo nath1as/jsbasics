878370a38ef6fbb8b162b9b99e7f1f0c06ffa46b
nathias <n@th1.as>
2018-07-05 21:49:10 +0200

    Fifth exercise done.

---
+++ 5.js
@@ -0,0 +1,22 @@
+var number1 = 23;
+var number2 = 17;
+
+console.log(number1 + ' + ' + number2 + ' = ' + (number1 + number2));
+console.log(number1 + ' - ' + number2 + ' = ' + (number1 - number2));
+console.log(number1 + ' * ' + number2 + ' = ' + (number1 * number2));
+console.log(number1 + ' / ' + number2 + ' = ' + Math.floor(number1 / number2));
+console.log(number1 + ' % ' + number2 + ' = ' + (number1 % number2));
+console.log(number1 + ' ** ' + number2 + ' = ' + Math.pow(number1, number2));
+
+// var number1 = prompt('Enter the first number:');
+// var number2 = prompt('Enter the second number:');
+
+// console.log(number1 + ' + ' + number2 + ' = ' + (number1 + number2));
+// console.log(number1 + ' - ' + number2 + ' = ' + (number1 - number2));
+// console.log(number1 + ' * ' + number2 + ' = ' + (number1 * number2));
+// console.log(number1 + ' / ' + number2 + ' = ' + Math.floor(number1 / number2));
+// console.log(number1 + ' % ' + number2 + ' = ' + (number1 % number2));
+// console.log(number1 + ' ** ' + number2 + ' = ' + Math.pow(number1, number2));
+
+// code works when vars are numbers and not if strings (as they would be when
+// prompted for)
