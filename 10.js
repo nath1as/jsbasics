e03abc2b035496b1c27c972f34765cd644e4d076
nathias <n@th1.as>
2018-07-09 13:17:33 +0200

    Tenth

---
+++ 10.js
@@ -0,0 +1,30 @@
+var DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+function integerToString(number) {
+  var result = '';
+  var remainder;
+
+  do {
+    remainder = number % 10;
+    number = Math.floor(number / 10);
+
+    result = DIGITS[remainder] + result;
+  } while (number > 0);
+
+  return result;
+}
+
+function signedIntegerToString(number) {
+  if (number < 0) {
+    return ('-' + integerToString(-number));
+  } else if (number > 0) {
+    return ('+' + integerToString(number));
+  } else {
+    return integerToString(number);
+  }
+}
+
+console.log(signedIntegerToString(4321));
+console.log(signedIntegerToString(0));
+console.log(signedIntegerToString(5000));
+
